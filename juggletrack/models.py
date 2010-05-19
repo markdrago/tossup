cabe1e29b694b8c66873749a55d2424469afa5a3
Mark Drago <markdrago@gmail.com>
2010-05-19 19:07:25 -0400

    setup models

--- juggletrack/models.py
+++ juggletrack/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Achievement(models.Model):
+    name = models.CharField(max_length=255)
+    points = models.IntegerField()
+    kind = models.CharField(max_length=255)
+    date_created = models.DateTimeField('date created')
+    
+    def __unicode__(self):
+        return self.name
+
+class Juggler(models.Model):
+    name = models.CharField(max_length=255)
+    date_created = models.DateTimeField('date created')
+    achievement = models.ManyToManyField(Achievement, through='JugglerAchievement')
+    
+    def __unicode__(self):
+        return self.name
+
+class JugglerAchievement(models.Model):
+    juggler = models.ForeignKey(Juggler)
+    achievement = models.ForeignKey(Achievement)
+    date_created = models.DateTimeField('date achieved')
+    
+    def __unicode__(self):
+        return self.juggler.name + ": " + self.achievement.name
+
